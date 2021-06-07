98b4c931995e6e54065178c4ad2d11e93037f785
cxx3668119 <862704268@qq.com>
2021-06-07 18:06:18 +0800

    first

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,58 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="getHandle">发送get请求</button>
+    <button @click="postHandle">发送post请求</button>
   </div>
 </template>
+
+
+<script>
+export default {
+  name: "About",
+  methods: {
+    //axios.get 发起get请求
+    //参数一表示请求地址
+    //参数二表示配置信息
+    //具体可见： http://www.axios-js.com/zh-cn/docs/
+    //params  表示传递到服务器端的数据，已url参数的形式拼接在请求地址后面
+    //{page:1,per:3}
+    // 比如:http://jsonplaceholder.typicode.com/
+    //最终生成：http://jsonplaceholder.typicode.com/?page=1&per=3
+    // 其中?表示可选参数
+    //headers  表示请求头
+    getHandle() {
+      this.axios
+        .get("https://api.cat-shop.penkuoer.com/api/v1/products", {
+          params: {
+            page: 3,
+            per: 2,
+          },
+          headers: {},
+        })
+        .then((res) => console.log(res));
+    },
+    postHandle() {
+      //post请求传递三个参数
+      //请求地址
+      //请求数据，请求配置，
+      //axios默认发送的数据是json格式的
+      //配置信息
+      //headers
+      //content-type:'application/json' 默认
+      this.axios
+        .post(
+          "https://api.cat-shop.penkuoer.com/api/v1/auth/login",
+          {
+            userName: "xiaoming",
+            password: "112132",
+          },
+          {}
+        )
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err));
+    },
+  },
+};
+</script>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  methods: {
+    //axios.get 发起get请求
+    //参数一表示请求地址
+    //参数二表示配置信息
+    //具体可见： http://www.axios-js.com/zh-cn/docs/
+    //params  表示传递到服务器端的数据，已url参数的形式拼接在请求地址后面
+    //{page:1,per:3}
+    // 比如:http://jsonplaceholder.typicode.com/
+    //最终生成：http://jsonplaceholder.typicode.com/?page=1&per=3
+    // 其中?表示可选参数
+    //headers  表示请求头
+  },
+};
 </script>
